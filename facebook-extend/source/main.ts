b3ffb93352c920563bc232f2d7bfce49e74974e9
Emil Rais <emilrais@outlook.com>
2017-12-18 15:12:32 +0100

    Prettified the main.ts of facebook-extend

--- facebook-extend/source/main.ts
+++ facebook-extend/source/main.ts
@@ -2,22 +2,20 @@ import { RequestHandler } from "express";
 
 import { Facebook } from "./facebook";
 
-export interface AbstractOperation {
+export interface Operation {
     module: string;
 }
 
-export const prepareOperation = (abstractOperation: AbstractOperation) => {
+export const prepareOperation = (operation: Operation) => {
     const appId = "1092068880930122";
     const appSecret = "470f440e050eb59788e7178c86ca982f";
     const facebook = new Facebook(appId, appSecret);
 
-    const concreteOperation: RequestHandler = (request, response, next) => {
+    return Promise.resolve<RequestHandler>((request, response, next) => {
         const userToken = request.body.token;
         facebook.extendToken(userToken)
             .then(extendedToken => response.locals.boards = extendedToken.access_token)
             .then(() => next())
             .catch(next);
-    };
-
-    return Promise.resolve(concreteOperation);
+    });
 };
