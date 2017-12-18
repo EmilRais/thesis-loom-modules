2b7376160106a83f2f28ccbd4fa8fe4ba4a0c325
Emil Rais <emilrais@outlook.com>
2017-12-18 15:12:20 +0100

    Prettified the main.ts of facebook-inspect

--- facebook-inspect/source/main.ts
+++ facebook-inspect/source/main.ts
@@ -6,7 +6,7 @@ export interface Operation {
     module: string;
 }
 
-export const prepareOperation = (abstractOperation: Operation) => {
+export const prepareOperation = (operation: Operation) => {
     const appId = "1092068880930122";
     const appSecret = "470f440e050eb59788e7178c86ca982f";
     const facebook = new Facebook(appId, appSecret);
